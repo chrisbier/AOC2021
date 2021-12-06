29e359f6ca125430bee7b3f4d8afdfb14f86008a
Chris Bier <github@cymor.com>
2021-12-05 21:54:21 -0600

    Day 5 part 1

---
+++ 05/code.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+# Day 05 part 1
+
+#import pprint
+
+#pp = pprint.PrettyPrinter(indent=4)
+
+inputfile = open('example.txt', 'r')
+input_list = inputfile.read().split('\n')
+
+class VentField:
+
+    field = {}
+    dangerous = 2
+
+    def __init__(self):
+        pass
+    
+    def __repr__(self):
+        output = ""
+        for x in self.field:
+            output_string_temp = "({},{}): {}\n"
+            output += output_string_temp.format(x[0], x[1], self.field[x])
+
+        return output
+
+    def add_line(self, start, end):
+        start_x = int(start[0])
+        start_y = int(start[1])
+        end_x = int(end[0])
+        end_y = int(end[1])
+    
+        # Verticle Line
+        if start_x == end_x:
+            if start_y < end_y:
+                y = start_y
+                y_end = end_y
+            else:
+                y = end_y
+                y_end = start_y
+
+            while y <= y_end:
+                new_value_temp = self.field.setdefault((start_x,y), 0) + 1
+                self.field[(start_x,y)] = new_value_temp
+                y+=1
+
+        # Horizontal Line
+        if start_y == end_y:
+            if start_x < end_x:
+                x = start_x
+                x_end = end_x
+            else:
+                x = end_x
+                x_end = start_x
+
+            while x <= x_end:
+                new_value_temp = self.field.setdefault((x,start_y), 0) + 1
+                self.field[(x,start_y)] = new_value_temp
+                x+=1
+        
+        # Diagonal
+        # Do nothing
+
+    def get_most(self):
+        # find most
+        most_value = max(self.field, key=self.field.get)
+        return self.field[most_value]
+
+    def get_all_most(self):
+        # find all most
+        #most_value = self.get_most()
+        return [ x for x in self.field if self.field[x] >= self.dangerous ]
+
+
+
+vent_field = VentField()
+
+#vent_field.add_line((1,5), (1,9))
+#vent_field.add_line((2,5), (0,5))
+
+# Process
+for line in input_list:
+    (first_cor, second_cor) = line.split(' -> ')
+    first_tup = tuple(first_cor.split(','))
+    second_tup = tuple(second_cor.split(','))
+    
+    vent_field.add_line(first_tup, second_tup)
+
+#print(vent_field)
+print(vent_field.get_most())
+all_dangerous = vent_field.get_all_most()
+print(len(all_dangerous))
